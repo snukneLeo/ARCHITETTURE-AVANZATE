bb2294323b5c85ec7ae98ea3375da380ecaeb3f5
leonardo@leo <leonardo.testolin@studenti.univr.it>
2019-07-18 10:21:21 +0200

    upload solutions

--- Esempi_MPI1/SendReceive/SendReceive.cpp
+++ Esempi_MPI1/SendReceive/SendReceive.cpp
@@ -26,9 +26,14 @@ int main(int argc, char* argv[]) {
         std::printf("Hello. This is %s. I'm sending the message..\n",
                     processor_name);
         
+        //int chisono = MPI::COMM_WORLD.Get_rank();
+
+        printf("Chi sono io: %i \n",world_rank);
+
         char buffer[] = "Hello! from rank 0";
         int count = std::strlen(buffer) + 1;
         int dest = 1;
+        
 
         int tag = 0;
         MPI::COMM_WORLD.Send(&count, 1, MPI::INT, dest, tag);
@@ -38,6 +43,8 @@ int main(int argc, char* argv[]) {
         MPI::COMM_WORLD.Send(&name_len, 1, MPI::INT, dest, tag);
         tag++;
         MPI::COMM_WORLD.Send(processor_name, name_len, MPI::CHAR, dest, tag);
+        tag++;
+        MPI::COMM_WORLD.Send("ciao franco F", 14, MPI::CHAR,dest,tag);
 
     }
     else if (world_rank == 1) {
@@ -45,6 +52,7 @@ int main(int argc, char* argv[]) {
         char processor_name[MPI_MAX_PROCESSOR_NAME];
         int count, name_len;
         int source = 0;
+        char buffer2[100];
 
         int tag = 0;
         MPI::COMM_WORLD.Recv(&count, 1, MPI::INT, source, tag);
@@ -54,10 +62,15 @@ int main(int argc, char* argv[]) {
         MPI::COMM_WORLD.Recv(&name_len, 1, MPI::INT, source, tag);
         tag++;
         MPI::COMM_WORLD.Recv(processor_name, name_len, MPI::CHAR, source, tag);
+        tag++;
+        MPI::COMM_WORLD.Recv(buffer2,14,MPI::CHAR,source,tag);
 
         // Print off a hello world message
         std::printf("Hello. this is %s: Message Received: \"%s\" of size %d from %s\n",
                     processor_name, buffer, count, processor_name);
+        // Print off a hello world message
+        std::printf("Hello. this is %s: Message Received: \"%s\" of size %d from %s\n",
+                    processor_name, buffer2, count, processor_name);
     }
     else
     std::printf("Hello. this is %s: I'm not playing... \n",
